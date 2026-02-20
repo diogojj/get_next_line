ccb44cfa30e3a15e6a4636082bb55d76c8e7411b
diogojj <47106217+diogojj@users.noreply.github.com>
2026-02-20 10:05:12 +0000

    Add function prototypes for helper functions

--- get_next_line.c
+++ get_next_line.c
@@ -11,6 +11,9 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+char	*_fill_line_buffer(int fd, char *left_c, char *buffer);
+char	*_set_line(char *line);
+char	*ft_strchr(char *s, int c);
 
 char    *get_next_line(int fd)
 {
